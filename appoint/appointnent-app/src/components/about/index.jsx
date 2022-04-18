00388ce61b097fb90c71db2c437702f983077b9f
Adrian649 <adrian.irizarry@upr.edu>
2022-04-18 18:35:36 -0400

    Added a bit of styling to about section

--- appoint/appointnent-app/src/components/about/index.jsx
+++ appoint/appointnent-app/src/components/about/index.jsx
@@ -12,7 +12,6 @@ const AboutCard = (props) => {
 
     return (
         <div className="card">
-            <div className="role">{props.role}</div>
             <img 
                 src={`${props.profile}`} 
                 className="card-image"
@@ -21,14 +20,28 @@ const AboutCard = (props) => {
             <div className="card-stats">
                 
                 
-                <span><a src={props.github}><FontAwesomeIcon icon={faGithub}/></a> •</span>
-                <span><a src={props.linkedin}><FontAwesomeIcon icon={faLinkedin}/></a> •</span>
-                <span><a src={props.twitter}><FontAwesomeIcon icon={faTwitter}/></a></span>
+                <span><a className='about-link' src={props.github}><FontAwesomeIcon icon={faGithub}/></a> •</span>
+                <span><a className='about-link' src={props.linkedin}><FontAwesomeIcon icon={faLinkedin}/></a> •</span>
+                <span><a className='about-link' src={props.twitter}><FontAwesomeIcon icon={faTwitter}/></a></span>
             </div>
             <div>
-                <h3>{props.name}</h3>
-                <p>{props.about}</p>
+                <h3 className='dev-name'>{props.name}</h3>
+                <p className="role">{props.role}</p>
+                <p className='dev-about'>{props.about}</p>
             </div>
+        </div>
+    );
+}
+
+const AboutHeader = () => {
+
+    return (
+        <div className='about-header-container'>
+            <h1 className='about-header-title'> Who are we? </h1>
+            <p className='about-header-p'>We are a group of students from the University of Puerto Rico: Mayaguez who are working on a project to bring a website that will serve doctors and patients alike.<br/> 
+            Our mission is to create a platform where any user will be able to easily set up appointments with medical professionals, while also providing these professionals <br/>
+            with an easy way to access appointments times to help keep track of each appointment.  </p>
+            <h2 className='about-header-title'>Meet the team</h2>
         </div>
     );
 }
@@ -45,17 +58,17 @@ export const About = () => {
             github: "https://github",
             profile: "",
             role: "Backend",
-            about: "A 5th year student on his Somophore year in Computer Enginnering. The responsible for the backend side of the project, as well as some minor things in the frontend. Don't ask him how it works, has no idea of anything."
+            about: "A 5th year student on his Sophmore year in Computer Enginnering. Responsible for the backend side of the project, as well as some minor things in the frontend. Don't ask him how it works, has no idea of anything."
         },
         {
             id: 2,
-            name: "Alejandro",
+            name: "Alejandro J Figueroa",
             picName: 'alejandro',
             nickname: "Alejo",
             linkedIn: "http://",
             github: "https://github",
             role: "Frontend",
-            about: "The responsible for the design and development of the UX/UI side of the app",
+            about: "Responsible for the design and development of the UX/UI side of the app",
             profile: ""
         },
         {
@@ -67,7 +80,7 @@ export const About = () => {
         },
         {
             id: 4,
-            name: "Noel",
+            name: "Noel Vargas",
             picName: 'noel',
             role: "Scheduler",
             about: "A member of the CAHSI Asociation, always keeps taps on the projects' progress"
@@ -75,27 +88,46 @@ export const About = () => {
         {
             id: 5,
             name: "Andrés Rodriguez",
-            picName: 'andres'
+            picName: 'andres',
+            role: 'Documentation',
+            about: 'Responsible for correcting and fixing anything wrong in the project'
 
+        },
+        {
+            id: 6,
+            name: "Adrian Irizarry",
+            picName: 'adrian',
+            role: 'Frontend',
+            about: '3rd year software engineer student. Responsible for helping with the frontend side of the project.',
+            github: 'https://github'
         }
     ]
 
-    ok.map( (e) => {
-        team.map( (j) => {
-            
-            if (e.includes(j.picName)) {
-                j.profile = e
-            }
-        })        
-    } )
+
+
 
     return (
+        <div className='about-wrapper'>
+            <AboutHeader/>
+
+
+            {
+                        ok.map( (e) => {
+                            team.map( (j) => {
+                                
+                                if (e.includes(j.picName)) {
+                                    j.profile = e
+                                }
+                            })        
+                        } )
+                    }
         <div className="about-container">
             {
                 team.map((member) => (   
                         AboutCard(member)
                 ))
             }
+        </div>
         </div>
     )
 }
